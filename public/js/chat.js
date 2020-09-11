3dabf34e543e29ce4f59bc0e4b4ac761c100545c
HieuVV <hieuvv@fabbi.io>
2020-09-11 17:18:48 +0700

    second commit

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,77 @@
-var socket = io()
+const socket = io()
+
+//Elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocationButton = document.querySelector('#send-location')
+const $messages = document.querySelector('#messages')
+
+
+
+//Templates
+
+const messageTemplate = document.querySelector('#message-template').innerHTML
+const messageLocationTemplate = document.querySelector('#message-location-template').innerHTML
+////
+
+socket.on('message', message => {
+    console.log(message)
+    const html = Mustache.render(messageTemplate, {
+        messageMustache: message.text,
+        createdAt: moment(message.createdAt).format('h:mm a')
+    });
+    $messages.insertAdjacentHTML('beforeend', html)
+})
+
+socket.on('messageLocation', url => {
+    console.log(url)
+
+    const html = Mustache.render(messageLocationTemplate, {
+        url: url.text,
+        createdAt: moment(url.createdAt).format('h:mm a')
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+})
+
+
+//////////////////////////////////
+$messageForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const message = $messageFormInput.value;
+
+    $messageFormButton.setAttribute('disabled', 'disabled')
+
+    //const message = e.target.elements.message.value
+    socket.emit('sendMessage', message , (error) => {
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = '';
+        $messageFormInput.focus();
+        if(error) {
+            return console.log(error)
+        }
+        console.log('Message delivered!')
+    })
+
+})
+
+
+//////////////////////////////////////
+$sendLocationButton.addEventListener('click', () => {
+
+    $sendLocationButton.setAttribute('disabled', 'disabled')
+    if(!navigator.geolocation) {
+        alert('your browser does not support geolocation')
+    }
+
+    navigator.geolocation.getCurrentPosition(location => {
+        console.log(location);
+        socket.emit('sendLocation', {
+            longitude: location.coords.longitude,
+            latitude: location.coords.latitude
+        }, () => {
+            console.log('Location printed')
+            $sendLocationButton.removeAttribute('disabled');
+        })
+    })
+})
--- src/index.js
+++ src/index.js
@@ -2,18 +2,46 @@ const express = require('express')
 const path = require('path')
 const app = express();
 const http = require('http')
-const socketio = require('socket.io');
-
+const Filter = require('bad-words');
+const generateMessage = require('./utils/generateMessage');
 
 const server = http.createServer(app)
-const io = socketio(server)
+const io = require('socket.io')(server);
+
+
+//const io = socketio(server)
 const port = process.env.PORT || 3000
 
 const publicDirectoryPath = path.join(__dirname, '../public');
 app.use(express.static(publicDirectoryPath))
 
+
+//---------------------------------------------
+let count = 0;
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    socket.emit('message', generateMessage('Wellcome'))
+    socket.broadcast.emit('message', generateMessage('An user have joined'))
+
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter();
+        if(filter.isProfane(message)) {
+          callback('Bad words not allowed')
+          return   io.emit('message', generateMessage('***********'))
+        }
+       io.emit('message', generateMessage(message))
+       callback();
+    })
+
+    socket.on('sendLocation', (coords, callback) => {
+        io.emit('messageLocation', generateMessage(`https://www.google.com/maps?q=${coords.latitude},${coords.longitude}`));
+        callback()
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', generateMessage('An user have left'))
+    })
+
+
 })
 
 
